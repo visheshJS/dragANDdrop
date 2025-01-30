4688017094b8d9fd2055d9c710f5f4ce94bdabf5
visheshJS <visheshsharma_23me287@dtu.ac.in>
2025-01-31 02:42:07 +0530

    files seperated

---
+++ script.js
@@ -0,0 +1,28 @@
+const lists = document.getElementsByClassName("list");
+const rightbox = document.getElementById("box2");
+const leftbox = document.getElementById("box1");
+
+for (const list of lists) {
+    list.addEventListener("dragstart",function(e){
+        let selected=e.target;
+
+        rightbox.addEventListener("dragover",function(e){
+            e.preventDefault();
+        });
+        
+        rightbox.addEventListener("drop",function(e){
+            rightbox.appendChild(selected);
+            selected=null;
+        });
+        leftbox.addEventListener("dragover",function(e){
+            e.preventDefault();
+        });
+        
+        leftbox.addEventListener("drop",function(e){
+            leftbox.appendChild(selected);
+            selected=null;
+        });
+    })
+    
+    
+}
